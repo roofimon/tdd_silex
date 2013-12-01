edb6634916517a7929ae4ac01998ec2a35a26fc6
roofimon <roofimon@gmail.com>
2013-12-01 16:25:50 +0700

    Test listWine return json

--- tests/WineControllerUnitTest.php
+++ tests/WineControllerUnitTest.php
@@ -12,16 +12,19 @@ use Symfony\Component\HttpFoundation\Request;
 
 class WineControllerUnitTest extends PHPUnit_Framework_TestCase {
     function testListWine(){
-
+        $expected_json = '[{"id":1,"title":"wine a"},{"id":2,"title":"some wine"}]';
 
         $mockServicePDO = $this->getMock('WineServicePDO', ['listWine']);
-        $mockServicePDO->expects($this->once())->method('listWine');
+        $mockServicePDO->expects($this->once())
+            ->method('listWine')
+            ->will($this->returnValue([['id'=>1, 'title'=>'wine a'], ['id'=>2, 'title'=>'some wine']]));
         $appMock = array('wine_service_pdo'=>$mockServicePDO);
         $wineController =  new WineController($appMock);
 
         $mockApplication = $this->getMock('Silex\Application');
         $mockRequest = $this->getMock('Symfony\Component\HttpFoundation\Request');
 
-        $wineController->listWine($mockRequest, $mockApplication);
+        $actual_json = $wineController->listWine($mockRequest, $mockApplication);
+        $this->assertEquals($expected_json, $actual_json);
     }
 } 
