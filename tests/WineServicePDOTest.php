85d1f678d179a7a32f2e5385dfc2f4e83acc42ff
Roof Panichsombat <roofimon@gmail.com>
2013-12-01 22:35:24 +0700

    Add addWine to ServicePDO

--- tests/WineServicePDOTest.php
+++ tests/WineServicePDOTest.php
@@ -4,31 +4,54 @@ require_once '../web/Wine.php';
 
 class WineServicePDOTest extends \PHPUnit_Framework_TestCase {
 
-    public function testListWine() {
-        $mockStatement = $this->getMock('PDOStatement', ['fetchAll']);
-        $mockStatement->expects($this->once())->method('fetchAll');
-        $mockPDO = $this->getMock('MockPDO', ['query']);
-        $mockPDO->expects($this->once())->method('query')->will($this->returnValue($mockStatement));
+  public function testListWine() {
+    $mockStatement = $this->getMock('PDOStatement', ['fetchAll']);
+    $mockStatement->expects($this->once())->method('fetchAll');
+    $mockPDO = $this->getMock('MockPDO', ['query']);
+    $mockPDO->expects($this->once())->method('query')->will($this->returnValue($mockStatement));
 
-        $wineAPI = new WineServicePDO();
-        $wineAPI->setPDO($mockPDO);
-        $wineAPI->listWine();
-    }
+    $wineAPI = new WineServicePDO();
+    $wineAPI->setPDO($mockPDO);
+    $wineAPI->listWine();
+  }
 
-    public function testAddWine() {
-        $mockStatement = $this->getMock('PDOStatement', ['execute']);
-        $mockStatement->expects($this->once())->method('execute');
+  public function testAddWine() {
+    $mockStatement = $this->getMock('PDOStatement', ['execute']);
+    $mockStatement->expects($this->once())->method('execute');
 
-        $mockPDO = $this->getMock('MockPDO', ['prepare']);
-        $mockPDO->expects($this->once())->method('prepare')->will($this->returnValue($mockStatement));
+    $mockPDO = $this->getMock('MockPDO', ['prepare']);
+    $mockPDO->expects($this->once())->method('prepare')->will($this->returnValue($mockStatement));
 
-        $wineAPI = new WineServicePDO();
-        $wineAPI->setPDO($mockPDO);
+    $wineAPI = new WineServicePDO();
+    $wineAPI->setPDO($mockPDO);
 
-        $wine = new Wine(['title'=>'new wine']);
-        $added_wine = $wineAPI->addWine($wine);
+    $wine = new Wine(['title'=>'new wine']);
+    $added_wine = $wineAPI->addWine($wine);
 
-        $this->assertEquals(1, $added_wine->getId());
-    }
+    $this->assertEquals(1, $added_wine->getId());
+  }
+
+
+  public function testGetWine() {
+    //arrange
+    $mockStatement = $this->getMock('PDOStatement', ['fetch']);
+    $mockStatement->expects($this->once())
+      ->method('fetch')
+      ->will($this->returnValue(new Wine(['title'=>'target wine'])));
+
+    $mockPDO = $this->getMock('MockPDO', ['prepare']);
+    $mockPDO->expects($this->once())->method('prepare')->will($this->returnValue($mockStatement));
+
+    $wineAPI = new WineServicePDO();
+    $wineAPI->setPDO($mockPDO);
+
+    $wineId = 1;
+    //act
+    $target_wine = $wineAPI->getWine($wineId);
+
+    //assert
+    $this->assertEquals(1, $target_wine->getId());
+  }
+
 
 }
--- web/WineServicePDO.php
+++ web/WineServicePDO.php
@@ -1,57 +1,71 @@
 <?php
 
 class WineServicePDO {
-    public $dbh;
+  public $dbh;
 
-    function getConnection() {
-        $dbhost="127.0.0.1";
-        $dbuser="root";
-        $dbpass="";
-        $dbname="wines";
-        if ($this->dbh == null) {
-            $this->dbh = new PDO("mysql:host=$dbhost;dbname=$dbname", $dbuser, $dbpass);
-            $this->dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        }
-        return $this->dbh;
+  function getConnection() {
+    $dbhost="127.0.0.1";
+    $dbuser="root";
+    $dbpass="";
+    $dbname="wines";
+    if ($this->dbh == null) {
+      $this->dbh = new PDO("mysql:host=$dbhost;dbname=$dbname", $dbuser, $dbpass);
+      $this->dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     }
+    return $this->dbh;
+  }
 
-    function listWine() {
-        try {
-            $sql = "select * from wines";
-            $dbh = $this->getConnection();
-            $stmt = $dbh->query($sql);
-            $wines = $stmt->fetchAll(PDO::FETCH_OBJ);
-            $dbh = null;
-            return $wines;
-        } catch (PDOException $e) {
-            echo $e->getMessage();
-        }
+  function listWine() {
+    try {
+      $sql = "select * from wines";
+      $dbh = $this->getConnection();
+      $stmt = $dbh->query($sql);
+      $wines = $stmt->fetchAll(PDO::FETCH_OBJ);
+      $dbh = null;
+      return $wines;
+    } catch (PDOException $e) {
+      echo $e->getMessage();
     }
+  }
 
-    function addWine($wine) {
-        $properties = ['title', 'grapes', 'country', 'price'];
-        $prepare_columns = implode(',', $properties);
-        $prepare_values = ':' . implode(', :', $properties);
-        try {
-            $sql = "insert into wines(" . $prepare_columns . ") values (" . $prepare_values . ")";
-            $db = $this->getConnection();
-            $stm = $db->prepare($sql);
-            $this->bindData($stm, (array)$wine, $properties);
-            $stm->execute();
-            return $wine;
-        } catch (PDOException $e) {
-            echo $e->getMessage();
-        }
+  function addWine($wine) {
+    $properties = ['title', 'grapes', 'country', 'price'];
+    $prepare_columns = implode(',', $properties);
+    $prepare_values = ':' . implode(', :', $properties);
+    try {
+      $sql = "insert into wines(" . $prepare_columns . ") values (" . $prepare_values . ")";
+      $db = $this->getConnection();
+      $stm = $db->prepare($sql);
+      $this->bindData($stm, (array)$wine, $properties);
+      $stm->execute();
+      return $wine;
+    } catch (PDOException $e) {
+      echo $e->getMessage();
     }
+  }
 
-    function bindData($statement, $data, $properties) {
-        foreach ($properties as $property_name) {
-            $statement->bindParam($property_name, $data[$property_name]);
-        }
+  function getWine($wineId){
+    try {
+      $sql = "select * from wines where id = :wineId";
+      $dbh = $this->getConnection();
+      $stmt = $dbh->prepare($sql);
+      $stmt->execute(array($wineId));
+      $wine = $stmt->fetch(PDO::FETCH_OBJ);
+      $dbh = null;
+      return $wine;
+    } catch (PDOException $e) {
+      echo $e->getMessage();
     }
+  }
 
-    public function setPDO($dbh) {
-        $this->dbh = $dbh;
+  function bindData($statement, $data, $properties) {
+    foreach ($properties as $property_name) {
+      $statement->bindParam($property_name, $data[$property_name]);
     }
+  }
+
+  public function setPDO($dbh) {
+    $this->dbh = $dbh;
+  }
 
 }
