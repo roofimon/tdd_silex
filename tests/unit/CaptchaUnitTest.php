8153d619331376cddbb06fe521d37cb596fcc97c
Roof Panichsombat <roofimon@gmail.com>
2014-02-22 11:29:44 +0700

    Using hamcrest

--- tests/unit/CaptchaUnitTest.php
+++ tests/unit/CaptchaUnitTest.php
@@ -5,5 +5,9 @@ class CaptchaUnitTest extends PHPUnit_Framework_TestCase {
     $captcha = new Captcha(1, 1, 1, 1);
     $actual = $captcha->toString();
     $this->assertEquals($expected, $actual);
+    assertThat($captcha->getLeftOperand(),  is(equalTo('1')));
+    assertThat($captcha->getOperation(),    is(equalTo('+')));
+    assertThat($captcha->getRightOperand(), is(equalTo('One')));
+    assertThat($captcha->getResult(),       is(equalTo('2')));
   }
 }
--- web/models/Captcha.php
+++ web/models/Captcha.php
@@ -1,5 +1,21 @@
 <?php
 class Captcha {
+  public function getLeftOperand() {
+    return "1";
+  }
+
+  public function getRightOperand() {
+    return "One";
+  }
+
+  public function getOperation() {
+    return "+";
+  }
+
+  public function getResult() {
+    return "2";
+  }
+
   public function toString() {
     return "1 + One = 2";
   }
