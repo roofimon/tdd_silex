562fed333b0bc91f8b53c36ca97c1364e2a6c5e3
Roof Panichsombat <roofimon@gmail.com>
2014-02-23 08:17:33 +0700

    Change method name from getOperation to getOperator

--- tests/unit/CaptchaUnitTest.php
+++ tests/unit/CaptchaUnitTest.php
@@ -6,9 +6,31 @@ class CaptchaUnitTest extends PHPUnit_Framework_TestCase {
     $actual = $captcha->toString();
     $this->assertEquals($expected, $actual);
     assertThat($captcha->getLeftOperand(),  is(equalTo('1')));
-    assertThat($captcha->getOperation(),    is(equalTo('+')));
+    assertThat($captcha->getOperator(),    is(equalTo('+')));
     assertThat($captcha->getRightOperand(), is(equalTo('One')));
     assertThat($captcha->getResult(),       is(equalTo('2')));
+  }
+
+  function testFirstPattern1PlusNineEquals10() {
+    $expected = "1 + Nine = 10";
+    $captcha = new Captcha(1, 1, 1, 9);
+    $actual = $captcha->toString();
+    $this->assertEquals($expected, $actual);
+    assertThat($captcha->getLeftOperand(),  is(equalTo('1')));
+    assertThat($captcha->getOperator(),    is(equalTo('+')));
+    assertThat($captcha->getRightOperand(), is(equalTo('Nine')));
+    assertThat($captcha->getResult(),       is(equalTo('10')));
+  }
+
+  function testFirstPattern7PlusEightEquals15() {
+    $expected = "7 + Eight = 15";
+    $captcha = new Captcha(1, 7, 1, 8);
+    $actual = $captcha->toString();
+    $this->assertEquals($expected, $actual);
+    assertThat($captcha->getLeftOperand(),  is(equalTo('7')));
+    assertThat($captcha->getOperator(),    is(equalTo('+')));
+    assertThat($captcha->getRightOperand(), is(equalTo('Eight')));
+    assertThat($captcha->getResult(),       is(equalTo('15')));
   }
 
   function testSecondPatternOnePlus1Equals2() {
@@ -17,7 +39,7 @@ class CaptchaUnitTest extends PHPUnit_Framework_TestCase {
     $actual = $captcha->toString();
     $this->assertEquals($expected, $actual);
     assertThat($captcha->getLeftOperand(),  is(equalTo('One')));
-    assertThat($captcha->getOperation(),    is(equalTo('+')));
+    assertThat($captcha->getOperator(),    is(equalTo('+')));
     assertThat($captcha->getRightOperand(), is(equalTo('1')));
     assertThat($captcha->getResult(),       is(equalTo('2')));
   }
@@ -28,8 +50,19 @@ class CaptchaUnitTest extends PHPUnit_Framework_TestCase {
     $actual = $captcha->toString();
     $this->assertEquals($expected, $actual);
     assertThat($captcha->getLeftOperand(),  is(equalTo('One')));
-    assertThat($captcha->getOperation(),    is(equalTo('+')));
+    assertThat($captcha->getOperator(),    is(equalTo('+')));
     assertThat($captcha->getRightOperand(), is(equalTo('2')));
     assertThat($captcha->getResult(),       is(equalTo('3')));
+  }
+
+  function testSecondPatternNinePlus9Equals18() {
+    $expected = "Nine + 9 = 18";
+    $captcha = new Captcha(2, 9, 1, 9);
+    $actual = $captcha->toString();
+    $this->assertEquals($expected, $actual);
+    assertThat($captcha->getLeftOperand(),  is(equalTo('Nine')));
+    assertThat($captcha->getOperator(),    is(equalTo('+')));
+    assertThat($captcha->getRightOperand(), is(equalTo('9')));
+    assertThat($captcha->getResult(),       is(equalTo('18')));
   }
 }
--- web/models/Captcha.php
+++ web/models/Captcha.php
@@ -2,7 +2,7 @@
 class Captcha {
   private $NUMBER_OPERATOR_TEXT = 1; 
   private $TEXT_OPERATOR_NUMBER = 2;
-  private $captchaString = array(1=>"One", 2=>"Two");
+  private $captchaString = array(1=>"One", 2=>"Two", 3=>"Three", 4=>"Four", 5=>"Five", 6=>"Six", 7=>"Seven", 8=>"Eight", 9=>"Nine");
 
   public function __construct($pattern, $leftOperand, $operator, $rightOperand) {
     $this->pattern = $pattern;
@@ -21,7 +21,7 @@ class Captcha {
     return $this->captchaString[$this->rightOperand];
   }
 
-  public function getOperation() {
+  public function getOperator() {
     return "+";
   }
 
